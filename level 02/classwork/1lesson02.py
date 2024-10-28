9fa144ae3e402fecd64ae9d01b8e51b63950e3cd
Roma-u <donghomski@gmail.com>
2024-10-28 20:38:50 +0400

    Add files via upload

---
+++ level 02/classwork/1lesson02.py
@@ -0,0 +1,9 @@
+# print('I am Roma Chekurashvili, 11 years old ')
+
+# name = 'Roma'
+# age = 11
+# surname = 'Chekurashvili'
+
+# print(name)
+# print(surname)
+# print(age)
---
+++ level 02/homework/1lesson02.py
@@ -0,0 +1,12 @@
+# print('Hello world')
+# print('GOA')
+# print('FUTURE CREW LEADER')
+# print('My crew leader best')
+# print('Cant wait for the next lesson')
+
+
+# animal = 'Duck'
+# transport = 'Bus'
+# school = 'N6 public'
+
+# print(school, animal, transport)
