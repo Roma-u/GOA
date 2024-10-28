cc7e72b633929e8ce547cb1cf697bc4d056bf6b5
Roma-u <donghomski@gmail.com>
2024-10-28 21:04:46 +0400

    Add files via upload

---
+++ Level 04/classwork/1lesson04.py
@@ -0,0 +1,14 @@
+# name = 'Roma'
+# city = 'Tbilisi'
+
+# age = 11
+# grade = 6
+
+# num = 13.1
+# num1 = 15.7
+# print(num, num1,age, grade, name, city)
+
+
+# number1 = 45
+# number2 = 45
+# print(number1 ** number2, number1 % number2, number1 * number2, number1 - number2, number1 // number2)
---
+++ Level 04/homework/1lesson04.py
@@ -0,0 +1,4 @@
+
+# number1 = 45
+# number2 = 45
+# print(number1 ** number2, number1 % number2, number1 * number2, number1 - number2, number1 // number2)
